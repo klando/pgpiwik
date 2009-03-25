136bfa1a9050816f8ab95dcc634ea6e27c01a1b8
Cédric Villemain <cedric.villemain.debian@gmail.com>
2009-03-25 23:54:14 +0100

    remove update sql tuo UTF8 from Updater

--- core/Updates/0.2.33.php
+++ core/Updates/0.2.33.php
@@ -1,12 +1,6 @@
 <?php
 require_once "SitesManager/API.php";
 
-// alter table to set the utf8 collation
-$tablesToAlter = Piwik::getTablesInstalled(true);
-foreach($tablesToAlter as $table) {
-	Piwik_Query("ALTER TABLE `". $table . "` 
-				 CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci ");
-}
 // force regeneration of cache files as we add 'hosts' entry in it
 Piwik::setUserIsSuperUser();
 $allSiteIds = Piwik_SitesManager_API::getAllSitesId();
