fb16b48c8c0497913966e099ff1a0579ab310b4c
Cédric Villemain <cedric.villemain.debian@gmail.com>
2009-04-12 15:33:23 +0200

    adjust the README and the PDO_PGSQL.sql for install
    There were some odd behavior. Now everything must execute cleanly.

--- misc/PDO_PGSQL.sql
+++ misc/PDO_PGSQL.sql
@@ -1,7 +1,9 @@
 -- createuser -S -D -R -P piwik_user # create postgresql user 
 CREATE DATABASE piwik WITH TEMPLATE = template0 ENCODING = 'UTF8';
 ALTER DATABASE piwik OWNER TO piwik_user;
-\connect piwik_user
+\connect piwik 
+
+BEGIN;
 
 CREATE OR REPLACE FUNCTION hour(time with time zone) RETURNS integer AS '
  SELECT
@@ -39,3 +41,17 @@ SELECT $1 || $2 ;
  ' LANGUAGE 'SQL';
 
 CREATE SEQUENCE idarchive_seq;
+
+ALTER FUNCTION public.concat(text, text, text, text, text) OWNER TO piwik_user;
+ALTER FUNCTION public.concat(text, text, text, text) OWNER TO piwik_user;
+ALTER FUNCTION public.concat(text, text, text) OWNER TO piwik_user;
+ALTER FUNCTION public.concat(text, text) OWNER TO piwik_user;
+ALTER FUNCTION public."current_date"() OWNER TO piwik_user;
+ALTER FUNCTION public.hour(time with time zone) OWNER TO piwik_user;
+ALTER FUNCTION public.hour(timestamp with time zone) OWNER TO piwik_user;
+ALTER FUNCTION public.unix_timestamp(timestamp with time zone) OWNER TO piwik_user;
+ALTER TABLE public.idarchive_seq OWNER TO piwik_user;
+
+COMMIT;
+
+
