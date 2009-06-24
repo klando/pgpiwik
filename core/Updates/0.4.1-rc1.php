e6b5d372cd387c97c082e21e669771b96f5c9ef6
Cédric Villemain <cedric.villemain.debian@gmail.com>
2009-06-24 21:30:20 +0200

    fix the updater scripts

--- core/Updates/0.4.1-rc1.php
+++ core/Updates/0.4.1-rc1.php
@@ -1,6 +1,6 @@
 <?php
 
 Piwik_Query('ALTER TABLE '. Piwik::prefixTable('log_conversion') .' 
-				CHANGE idlink_va idlink_va int(11)  default NULL;');
+				ALTER COLUMN idlink_va DROP NOT NULL;');
 Piwik_Query('ALTER TABLE '. Piwik::prefixTable('log_conversion') .' 
-				CHANGE idaction idaction int(11)  default NULL;');
+				ALTER COLUMN idaction DROP NOT NULL;');
--- core/Updates/0.4.php
+++ core/Updates/0.4.php
@@ -1,9 +1,9 @@
 <?php
 
-Piwik_Query('UPDATE '. Piwik::prefixTable('log_visit') .' SET location_ip=location_ip+CAST(POW(2,32) AS UNSIGNED) WHERE location_ip < 0;');
-Piwik_Query('ALTER TABLE '. Piwik::prefixTable('log_visit') .' CHANGE location_ip location_ip BIGINT UNSIGNED;');
+Piwik_Query('UPDATE '. Piwik::prefixTable('log_visit') .' SET location_ip=location_ip+2^32 WHERE location_ip < 0;');
+Piwik_Query('ALTER TABLE '. Piwik::prefixTable('log_visit') .' ALTER COLUMN location_ip DROP NOT NULL;');
 
-Piwik_Query('UPDATE '. Piwik::prefixTable('logger_api_call') .' SET caller_ip=caller_ip+CAST(POW(2,32) AS UNSIGNED) WHERE caller_ip < 0;');
-Piwik_Query('ALTER TABLE '. Piwik::prefixTable('logger_api_call') .' CHANGE caller_ip caller_ip BIGINT UNSIGNED;');
+Piwik_Query('UPDATE '. Piwik::prefixTable('logger_api_call') .' SET caller_ip=caller_ip+2^32 WHERE caller_ip < 0;');
+Piwik_Query('ALTER TABLE '. Piwik::prefixTable('logger_api_call') .' ALTER COLUMN caller_ip DROP NOT NULL;');
 
 Piwik_Query( "ALTER TABLE ". Piwik::prefixTable('log_visit') . " DROP config_java" );
